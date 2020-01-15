8742e6d49797c1f5f81eada87c520c0607bfd366
marco_boffo <marco97.mb@gmail.com>
2020-01-15 10:49:02 +0100

    Removed Ros Templates

--- templateFiles/MATLAB/myControlAlgorithm.m
+++
@@ -1,8 +0,0 @@
-function ctrlOutput = myControlAlgorithm(ctrlInput,params)
-% Placeholder control algorithm
-% Copyright 2017 The MathWorks, Inc.
-
-    ctrlOutput = ctrlInput * params.gain;
-
-end
-
--- templateFiles/MATLAB/myPerceptionAlgorithm.m
+++
@@ -1,8 +0,0 @@
-function perceptionOutput = myPerceptionAlgorithm(recvData,param)
-% Placeholder perception algorithm
-% Copyright 2017 The MathWorks, Inc.
-
-    perceptionOutput = recvData / param; 
-
-end
-
